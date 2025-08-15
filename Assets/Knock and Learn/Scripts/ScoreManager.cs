fdaed0e75a6140c94ccb1ae4f19f199d413590ec
Nur Alif <BaseGratz@gmail.com>
2025-08-15 20:58:10 +0800

    Score Manager
    Created a script that tracks when a correct stack is hit to +1 score

---
+++ Assets/Knock and Learn/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateUI();
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (scoreText != null)
+            scoreText.text = "Score: " + score;
+    }
+}
